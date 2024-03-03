5c2d8813e03d8a10cfd193370e07330b3f957abe
tsuyuki77 <insalacaa@hotmail.com>
2024-03-03 17:49:15 +0100

    nieuwe versie

--- les02Dom1/TaskList/js/scripts.js
+++ les02Dom1/TaskList/js/scripts.js
@@ -1,24 +1,36 @@
-   const frm = document.querySelector('#frmTask');
-   const priority = frm.querySelector('#selPriority');
-   const inpDeadline = frm.querySelector('#datDeadline');
-   const inpTask = frm.querySelector('#txtTask');
-   const tasksContent = document.querySelector('#tasks');
-   frm.addEventListener('submit', function(e) {
-      e.preventDefault();
-      const icon = 'more_horiz';
-      const geschrevenTekst = inpTask.value;
-      const deadline = inpDeadline.value;
+const frm = document.querySelector('#frmTask');
+const priority = frm.querySelector('#selPriority');
+const inpDeadline = frm.querySelector('#datDeadline');
+const inpTask = frm.querySelector('#txtTask');
+const tasksContent = document.querySelector('#tasks');
+const thumbLinks = document.querySelectorAll('#tasks .complete');
 
-      let backgroundkleur = '';
-      if (priority.value == 'low') {backgroundkleur = 'green';
-      } else if (priority.value == 'normal') { backgroundkleur = 'orange';
-      } else if (priority.value == 'high') { backgroundkleur = 'red'; }
-
-      const nieuweTask = `<div class="task"> 
-         <span class="priority material-icons" style="color: ${backgroundkleur}">assignment</span> 
-         <p class="tasktext">${geschrevenTekst} <span class="deadline">(${deadline})</span></p> 
-         <span class="complete material-icons">${icon}</span>
-      </div>`;
-      tasksContent.innerHTML += nieuweTask;
+thumbLinks.forEach(lnk => {
+   lnk.addEventListener('mouseover', function() {
+      lnk.innerHTML = 'check';
+      lnk.classList.add('kleur-green');
    });
+   lnk.addEventListener('mouseout', function() {
+      lnk.innerHTML = 'more_horiz';
+      lnk.classList.remove('kleur-green');
+   });
+   lnk.addEventListener('click', function() {
+      lnk.parentNode.remove(); // Danzkij parentNode neem je het bovenste element. Zie uitleg -> https://www.w3schools.com/jsref/prop_node_parentnode.asp
+   });
+});
 
+frm.addEventListener('submit', function(e) {
+   e.preventDefault();
+   const geschrevenTekst = inpTask.value;
+   const deadline = inpDeadline.value;
+   let backgroundkleur = '';
+   if (priority.value == 'low') backgroundkleur = 'kleur-green';
+   else if (priority.value == 'normal') backgroundkleur = 'kleur-orange';
+   else if (priority.value == 'high') backgroundkleur = 'kleur-red';
+   const nieuweTask = `<div class="task"> 
+         <span class="priority material-icons ${backgroundkleur}">assignment</span> 
+         <p class="tasktext">${geschrevenTekst} <span class="deadline">(deadline: ${deadline})</span></p> 
+         <span class="complete material-icons">more_horiz</span>
+      </div>`;
+   tasksContent.innerHTML += nieuweTask;
+});

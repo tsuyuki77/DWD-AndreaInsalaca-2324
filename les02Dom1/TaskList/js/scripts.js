298cc69da3babaa0ba7b06663dc5dc6f5bab73a6
tsuyuki77 <insalacaa@hotmail.com>
2024-03-03 17:09:09 +0100

    nieuwe versie

--- les02Dom1/TaskList/js/scripts.js
+++ les02Dom1/TaskList/js/scripts.js
@@ -1,19 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() { // dankzij deze stukje code werkt het wel
    const frm = document.querySelector('#frmTask');
    const priority = frm.querySelector('#selPriority');
    const inpDeadline = frm.querySelector('#datDeadline');
    const inpTask = frm.querySelector('#txtTask');
-   const tasksContent = frm.querySelector('#tasks');
+   const tasksContent = document.querySelector('#tasks');
    frm.addEventListener('submit', function(e) {
       e.preventDefault();
-      let backgroundkleur = '';
       const icon = 'more_horiz';
       const geschrevenTekst = inpTask.value;
       const deadline = inpDeadline.value;
-      if (priority.value == 'low') { backgroundkleur = 'green'; }
-      else if (priority.value == 'normal') { backgroundkleur = 'orange'; }
-      else if (priority.value == 'high') { backgroundkleur = 'red'; }
-      const nieuweTask = `<div class="task"> <span class="priority material-icons" background-color: ${backgroundkleur} >assignment</span> <p class="tasktext"> ${geschrevenTekst} <span class="deadline">(${deadline})</span></p> <span class="complete material-icons">${icon}</span></div>`;
+
+      let backgroundkleur = '';
+      if (priority.value == 'low') {backgroundkleur = 'green';
+      } else if (priority.value == 'normal') { backgroundkleur = 'orange';
+      } else if (priority.value == 'high') { backgroundkleur = 'red'; }
+
+      const nieuweTask = `<div class="task"> 
+         <span class="priority material-icons" style="color: ${backgroundkleur}">assignment</span> 
+         <p class="tasktext">${geschrevenTekst} <span class="deadline">(${deadline})</span></p> 
+         <span class="complete material-icons">${icon}</span>
+      </div>`;
       tasksContent.innerHTML += nieuweTask;
    });
-});
+

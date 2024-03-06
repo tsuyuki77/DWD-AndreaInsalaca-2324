4cd10bc8861e3b5622cce8623211fb5507fe6ffb
tsuyuki77 <insalacaa@hotmail.com>
2024-03-06 22:31:23 +0100

    nieuwe versie

--- les02Dom1/TaskList/js/scripts.js
+++ les02Dom1/TaskList/js/scripts.js
@@ -3,22 +3,13 @@ const priority = frm.querySelector('#selPriority');
 const inpDeadline = frm.querySelector('#datDeadline');
 const inpTask = frm.querySelector('#txtTask');
 const tasksContent = document.querySelector('#tasks');
-const thumbLinks = document.querySelectorAll('#tasks .complete');
 
-thumbLinks.forEach(lnk => {
-   lnk.addEventListener('mouseover', function() {
-      lnk.innerHTML = 'check';
-      lnk.classList.add('kleur-green');
-   });
-   lnk.addEventListener('mouseout', function() {
-      lnk.innerHTML = 'more_horiz';
-      lnk.classList.remove('kleur-green');
-   });
-   lnk.addEventListener('click', function() {
-      lnk.parentNode.remove(); // Danzkij parentNode neem je het bovenste element. Zie uitleg -> https://www.w3schools.com/jsref/prop_node_parentnode.asp
-   });
+tasksContent.addEventListener('click', function(e) {
+   if (e.target.classList.contains('complete')) {
+      e.target.innerHTML = 'check';
+      e.target.classList.add('kleur-green');
+   }
 });
-
 frm.addEventListener('submit', function(e) {
    e.preventDefault();
    const geschrevenTekst = inpTask.value;

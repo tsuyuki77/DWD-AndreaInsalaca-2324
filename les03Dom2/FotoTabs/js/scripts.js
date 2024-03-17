63fcbf8073014a21fe1f819734cbbe060a70a7f3
tsuyuki77 <insalacaa@hotmail.com>
2024-03-17 11:34:44 +0100

    verbetering

--- les03Dom2/FotoTabs/js/scripts.js
+++ les03Dom2/FotoTabs/js/scripts.js
@@ -1,22 +1,22 @@
-const navFilters = document.querySelectorAll('.nav__filters a');
-const gridFig = document.querySelectorAll('figure');
+const lnksTabs = document.querySelectorAll('.nav__filters a');
+const figsPhotos = document.querySelectorAll('figure');
 const lnkView = document.querySelectorAll('.header__view a');
-const gridView = document.querySelector('#grid');
-navFilters.forEach(thn => {
+const grdPhotos = document.querySelector('#grid');
+const numfound = document.querySelector('#numFound');
+lnksTabs.forEach(thn => {
    thn.addEventListener('click', function(e) {
       e.preventDefault();
-      document.querySelector('.active').classList.remove('active'); // active verwijderen
+      document.querySelector('.nav__filters a.active').classList.remove('active'); // active verwijderen
       thn.classList.add('active');
 
       // geselecteerde kader tonen
-      gridFig.forEach(fig => {
-         const datafilter = thn.getAttribute('data-filter'); // om het leesbaarder te maken
-         if (fig.getAttribute('data-filters').includes(datafilter) || datafilter == 'alle') {
-            fig.classList.remove('hidden');
-         } else {
-            fig.classList.add('hidden');
-         }
+      figsPhotos.forEach(fig => {
+         const datafilter = thn.dataset.filter; // om het leesbaarder te maken
+         fig.classList.toggle('hidden', !fig.dataset.filters.includes(datafilter) && datafilter != 'alle');
       });
+
+      // toon aantal
+      numfound.innerHTML = figsPhotos.length - grdPhotos.querySelectorAll('figure.hidden').length;
    });
 });
 lnkView.forEach(act => {
@@ -27,11 +27,11 @@ lnkView.forEach(act => {
 
       if (e.target.innerHTML == 'view_module') {
          document.querySelector('#grid').classList.remove('viewList');
-         gridView.classList.add('viewGrid');
+         grdPhotos.classList.add('viewGrid');
       }
       if (e.target.innerHTML == 'view_list') {
          document.querySelector('#grid').classList.remove('viewGrid');
-         gridView.classList.add('viewList');
+         grdPhotos.classList.add('viewList');
       }
    });
 });
